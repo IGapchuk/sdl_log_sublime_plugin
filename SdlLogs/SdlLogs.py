e03ffdf965eeace46e1143a5edf8b915922b48be
Denys Kandyba <dkandyba@luxoft.com>
2018-11-06 13:59:49 +0200

    Answer to PR comments.

--- SdlLogs/SdlLogs.py
+++ SdlLogs/SdlLogs.py
@@ -16,7 +16,6 @@ threadEnter     = ".(cc|h):[0-9]{1,5} (.*): Enter"
 threadExit      = " Exit"
 threadRegex     = "\\[0x[a-zA-Z0-9]{12}]"
 pathRegex       = "sdl_core/src/(.*).(cc|h):[0-9]{1,5}" 
-fullPathFile	= "(/.*)(/sdl_core/src/.{0,}\\.(cc|h|cpp|hpp)):(\\d{1,})"
 
 #flags for commands
 actThreadFlag    = False
@@ -134,8 +133,27 @@ class FilterByValueCommand(sublime_plugin.TextCommand):
 
 class JumpToFileCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		primaryPath = re.search(fullPathFile, self.view.substr(self.view.line(self.view.sel()[0])))
-		self.view.window().open_file(self.view.settings().get(sourcePath) +
-			primaryPath.group(2) + ":" + primaryPath.group(4), sublime.ENCODED_POSITION)
+		FULL_PATH_FILE	= "(/.*)(/sdl_core/src/.{0,}\\.(cc|h|cpp|hpp)):(\\d{1,})"
+		CORRECTED_PATH  = "(/[^/ ]*)+/?"
 
+		primaryPath = re.search(FULL_PATH_FILE, self.view.substr(self.view.line(self.view.sel()[0])))
+		isItPath = re.search(CORRECTED_PATH, self.view.substr(self.view.line(self.view.sel()[0])))
 
+		if self.view.settings().get(sourcePath) != "": 
+			if primaryPath :
+				file = self.view.settings().get(sourcePath) + primaryPath.group(2)
+				if path.isfile(file) : 
+					self.view.window().open_file(file + ":" + primaryPath.group(4), sublime.ENCODED_POSITION)
+				else:
+					sublime.error_message("File '{0}' not found. Maybe path to source files in your settings is incorrect or file really absent".format(file))
+			elif isItPath:
+				sublime.error_message("Expected '/sdl_core/src/' in '{0}' ".format(isItPath.group(0)))
+		else:
+			if primaryPath :
+				file = primaryPath.group(1) + primaryPath.group(2)
+				if path.isfile(file): 
+					self.view.window().open_file(file + ":" + primaryPath.group(4), sublime.ENCODED_POSITION)
+				else:
+					sublime.error_message("File '{0}' not found. You can write path to source files in settings and try again. Or file really absent".format(file))
+			elif isItPath:
+				sublime.error_message("Expected '/sdl_core/src/' in '{0}' ".format(isItPath.group(0)))
